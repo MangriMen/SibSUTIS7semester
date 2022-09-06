efc194ce2c0199cbcd47e82cf9608a7334c2aa30
MangriMen <49300253+MangriMen@users.noreply.github.com>
2022-09-06 20:11:18 +0700

    ZI lab1 done

--- ZI/lab1/libtest/libtest.cpp
+++ ZI/lab1/libtest/libtest.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
-#include "../lab1/lab1.cpp"
+#include "../lab1/lab1.h"
+
+using namespace std;
 
 int main()
 {
-    int x = 0;
-    int y = 0;
-    std::cout << gcd(2, 4, x, y) << "\n";
-    std::cout << x << " " << y;
+	auto modExp_result = modExp(1, 2, 3);
+	cout << "Module Exp: " << modExp_result << "\n";
+
+	auto gcd_result = getExtendedGCD(2, 31);
+	cout << "Extended GCD: " << gcd_result[0] << "; x: " << gcd_result[1] << " y: " << gcd_result[2] << "\n";
+
+	for (int i = 0; i < 100000; i++) {
+		auto generateDiffieHellmanKey_result = generateDiffieHellmanKey();
+		cout << "Diffie Hellman: " << generateDiffieHellmanKey_result << "\n";
+	}
+
+	auto babyGiantStep_result = babyGiantStep(3, 31, 7);
+	cout << "Baby Giant Step: " << babyGiantStep_result << "\n";
 }
