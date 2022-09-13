cef5bd03271ba2b592e9aa75a957953b847ef0d1
IHiFiveI <57842399+IHiFiveI@users.noreply.github.com>
2022-09-13 11:54:39 +0700

    STP1 lab4 Tests finished

--- STP1/lab4/lab4.Tests/MatrixTest.cs
+++ STP1/lab4/lab4.Tests/MatrixTest.cs
@@ -1,5 +1,3 @@
-using lab4;
-
 namespace lab4.Tests
 {
     [TestClass]
@@ -30,7 +28,7 @@ namespace lab4.Tests
         public void TestIndexOperatorGetBigI()
         {
             Matrix testMatrix = new(2, 2);
-            int wrongValue = testMatrix[3, 1];
+            int _ = testMatrix[3, 1];
         }
 
         [TestMethod]
@@ -46,7 +44,7 @@ namespace lab4.Tests
         public void TestIndexOperatorGetBigJ()
         {
             Matrix testMatrix = new(2, 2);
-            int wrongValue = testMatrix[1, 3];
+            int _ = testMatrix[1, 3];
         }
 
         [TestMethod]
@@ -61,7 +59,7 @@ namespace lab4.Tests
         public void TestIndexOperatorGet()
         {
             Matrix testMatrix = new(2, 2);
-            int wrongValue = testMatrix[1, 1];
+            int _ = testMatrix[1, 1];
         }
 
         [TestMethod]
@@ -74,60 +72,282 @@ namespace lab4.Tests
         [TestMethod]
         public void TestSumOperator()
         {
-            Matrix firstMatrix = new((new int[,] { { 1, 1 }, { 1, 1 } }), 2, 2);
-            Matrix secondMatrix = new((new int[,] { { 2, 2 }, { 2, 2 } }), 2, 2);
+            Matrix firstMatrix = new(new int[,] { { 1, 1 }, { 1, 1 } }, 2, 2);
+            Matrix secondMatrix = new(new int[,] { { 2, 2 }, { 2, 2 } }, 2, 2);
 
-            Matrix expectedValue = new((new int[,] { { 3, 3 }, { 3, 3 } }), 2, 2);
+            Matrix expectedValue = new(new int[,] { { 3, 3 }, { 3, 3 } }, 2, 2);
             Matrix actualValue = firstMatrix + secondMatrix;
 
-            // Assert.myBalls.XD.fuckoff();
+            Assert.AreEqual(expectedValue, actualValue);
         }
 
-        // [TestMethod]
-        // public void Equel()
-        // {
-        //     //arrange(обеспечить)
-        //     Matrix a = new(2, 2);
-        //     a[0, 0] = 1;
-        //     a[0, 1] = 1;
-        //     a[1, 0] = 1;
-        //     a[1, 1] = 1;
-        //     Matrix b = new(2, 2);
-        //     b[0, 0] = 1;
-        //     b[0, 1] = 1;
-        //     b[1, 0] = 1;
-        //     b[1, 1] = 1;
-        //     //act (выполнить)
-        //     //bool r = a == b;
-        //     //assert(доказать)
-        //     //Assert.IsTrue(r);
-        //     Assert.AreEqual(a, b);
-        // }
+        [TestMethod]
+        public void TestSumOperatorNegativeTerms()
+        {
+            Matrix firstMatrix = new(new int[,] { { -7, 3 }, { -6, -1 } }, 2, 2);
+            Matrix secondMatrix = new(new int[,] { { -2, 2 }, { -2, -2 } }, 2, 2);
 
-        // [TestMethod]
-        // public void Summa()
-        // {
-        //     //arrange(обеспечить)
-        //     Matrix a = new(2, 2);
-        //     a[0, 0] = 1;
-        //     a[0, 1] = 1;
-        //     a[1, 0] = 1;
-        //     a[1, 1] = 1;
-        //     Matrix b = new(2, 2);
-        //     b[0, 0] = 2;
-        //     b[0, 1] = 2;
-        //     b[1, 0] = 2;
-        //     b[1, 1] = 2;
-        //     Matrix expected = new(2, 2);
-        //     expected[0, 0] = 3;
-        //     expected[0, 1] = 3;
-        //     expected[1, 0] = 3;
-        //     expected[1, 1] = 3;
-        //     Matrix actual = new(2, 2);
-        //     //act (выполнить)
-        //     actual = a + b;
-        //     //assert(доказать)
-        //     Assert.IsTrue(actual == expected); //Оракул
-        // }
+            Matrix expectedValue = new(new int[,] { { -9, 5 }, { -8, -3 } }, 2, 2);
+            Matrix actualValue = firstMatrix + secondMatrix;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestSumOperatorZeroes()
+        {
+            Matrix firstMatrix = new(new int[,] { { 0, 0 }, { 0, 0 } }, 2, 2);
+            Matrix secondMatrix = new(new int[,] { { 0, 0 }, { 0, 0 } }, 2, 2);
+
+            Matrix expectedValue = new(new int[,] { { 0, 0 }, { 0, 0 } }, 2, 2);
+            Matrix actualValue = firstMatrix + secondMatrix;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestMinusOperator()
+        {
+            Matrix firstMatrix = new(new int[,] { { 2, 2 }, { 2, 2 } }, 2, 2);
+            Matrix secondMatrix = new(new int[,] { { 1, 1 }, { 1, 1 } }, 2, 2);
+
+            Matrix expectedValue = new(new int[,] { { 1, 1 }, { 1, 1 } }, 2, 2);
+            Matrix actualValue = firstMatrix - secondMatrix;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestMinusOperatorNegativeResult()
+        {
+            Matrix firstMatrix = new((new int[,] { { 0, 0 }, { 0, 0 } }), 2, 2);
+            Matrix secondMatrix = new((new int[,] { { 1, 1 }, { 1, 1 } }), 2, 2);
+
+            Matrix expectedValue = new((new int[,] { { -1, -1 }, { -1, -1 } }), 2, 2);
+            Matrix actualValue = firstMatrix - secondMatrix;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestMinusOperatorZeroes()
+        {
+            Matrix firstMatrix = new(new int[,] { { 0, 0 }, { 0, 0 } }, 2, 2);
+            Matrix secondMatrix = new(new int[,] { { 0, 0 }, { 0, 0 } }, 2, 2);
+
+            Matrix expectedValue = new(new int[,] { { 0, 0 }, { 0, 0 } }, 2, 2);
+            Matrix actualValue = firstMatrix - secondMatrix;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestMultiplyOperator()
+        {
+            Matrix firstMatrix = new(new int[,] { { 3, 3 }, { 3, 3 } }, 2, 2);
+            Matrix secondMatrix = new(new int[,] { { 2, 2 }, { 2, 2 } }, 2, 2);
+
+            Matrix expectedValue = new(new int[,] { { 12, 12 }, { 12, 12 } }, 2, 2);
+            Matrix actualValue = firstMatrix * secondMatrix;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestMultiplyOperatorNegative()
+        {
+            Matrix firstMatrix = new(new int[,] { { 3, 3 }, { 3, -3 } }, 2, 2);
+            Matrix secondMatrix = new(new int[,] { { -2, 2 }, { 2, 2 } }, 2, 2);
+
+            Matrix expectedValue = new(new int[,] { { 0, 12 }, { -12, 0 } }, 2, 2);
+            Matrix actualValue = firstMatrix * secondMatrix;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestMultiplyOperatorZeroes()
+        {
+            Matrix firstMatrix = new(new int[,] { { 0, 0 }, { 0, 0 } }, 2, 2);
+            Matrix secondMatrix = new(new int[,] { { 0, 0 }, { 0, 0 } }, 2, 2);
+
+            Matrix expectedValue = new(new int[,] { { 0, 0 }, { 0, 0 } }, 2, 2);
+            Matrix actualValue = firstMatrix * secondMatrix;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestEqualityOperator()
+        {
+            Matrix firstMatrix = new(new int[,] { { 3, 3 }, { 3, 3 } }, 2, 2);
+            Matrix secondMatrix = new(new int[,] { { 3, 3 }, { 3, 3 } }, 2, 2);
+
+            bool expectedValue = true;
+            bool actualValue = firstMatrix == secondMatrix;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestEqualityOperatorNotEqual()
+        {
+            Matrix firstMatrix = new(new int[,] { { 3, 3 }, { 3, 3 } }, 2, 2);
+            Matrix secondMatrix = new(new int[,] { { 2, 2 }, { 2, 2 } }, 2, 2);
+
+            bool expectedValue = true;
+            bool actualValue = firstMatrix == secondMatrix;
+
+            Assert.AreNotEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestEqualityOperatorZeroes()
+        {
+            Matrix firstMatrix = new(new int[,] { { 0, 0 }, { 0, 0 } }, 2, 2);
+            Matrix secondMatrix = new(new int[,] { { 0, 0 }, { 0, 0 } }, 2, 2);
+
+            bool expectedValue = true;
+            bool actualValue = firstMatrix == secondMatrix;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+
+        [TestMethod]
+        public void TestNotEqualOperator()
+        {
+            Matrix firstMatrix = new(new int[,] { { 3, 3 }, { 3, 3 } }, 2, 2);
+            Matrix secondMatrix = new(new int[,] { { 3, 3 }, { 3, 3 } }, 2, 2);
+
+            bool expectedValue = true;
+            bool actualValue = firstMatrix != secondMatrix;
+
+            Assert.AreNotEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestNotEqualOperatorNotEqual()
+        {
+            Matrix firstMatrix = new(new int[,] { { 3, 3 }, { 3, 3 } }, 2, 2);
+            Matrix secondMatrix = new(new int[,] { { 2, 2 }, { 2, 2 } }, 2, 2);
+
+            bool expectedValue = true;
+            bool actualValue = firstMatrix != secondMatrix;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestNotEqualOperatorZeroes()
+        {
+            Matrix firstMatrix = new(new int[,] { { 0, 0 }, { 0, 0 } }, 2, 2);
+            Matrix secondMatrix = new(new int[,] { { 0, 0 }, { 0, 0 } }, 2, 2);
+
+            bool expectedValue = true;
+            bool actualValue = firstMatrix != secondMatrix;
+
+            Assert.AreNotEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestTranspose2x2()
+        {
+            Matrix testMatrix = new(new int[,] { { 1, 2 }, { 3, 4 } }, 2, 2);
+
+            Matrix expectedValue = new(new int[,] { { 1, 3 }, { 2, 4 } }, 2, 2);
+            Matrix actualValue = testMatrix.Tranpose();
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestTranspose3x3()
+        {
+            Matrix testMatrix = new(new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }, 3, 3);
+
+            Matrix expectedValue = new(new int[,] { { 1, 4, 7 }, { 2, 5, 8 }, { 3, 6, 9 } }, 3, 3);
+            Matrix actualValue = testMatrix.Tranpose();
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestMin()
+        {
+            Matrix testMatrix = new(new int[,] { { 1, 2 }, { 3, 4 } }, 2, 2);
+
+            int expectedValue = 1;
+            int actualValue = testMatrix.Min();
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestMinZero()
+        {
+            Matrix testMatrix = new(new int[,] { { 1, 2 }, { 0, 4 } }, 2, 2);
+
+            int expectedValue = 0;
+            int actualValue = testMatrix.Min();
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestMinNegative()
+        {
+            Matrix testMatrix = new(new int[,] { { -651, 2 }, { 3, 4 } }, 2, 2);
+
+            int expectedValue = -651;
+            int actualValue = testMatrix.Min();
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestToString() {
+            string expectedValue = "{{1,2},{3,4}}";
+            Matrix actualValue = new(new int[,] { { 1, 2 }, { 3, 4 } }, 2, 2);
+
+            Assert.AreEqual(expectedValue, actualValue.ToString());
+            Assert.IsInstanceOfType(actualValue.ToString(), typeof(string));
+        }
+
+        [TestMethod]
+        public void TestToStringEmptyMatrix()
+        {
+            string expectedValue = "{{0}}";
+            Matrix actualValue = new(1, 1);
+
+            Assert.AreEqual(expectedValue, actualValue.ToString());
+            Assert.IsInstanceOfType(actualValue.ToString(), typeof(string));
+        }
+
+        [TestMethod]
+        public void TestEquals()
+        {
+            Matrix firstMatrix = new(new int[,] { { 2, 2 }, { 2, 2 } }, 2, 2);
+            Matrix secondMatrix = new(new int[,] { { 2, 2 }, { 2, 2 } }, 2, 2);
+
+            bool expectedValue = true;
+            bool actualValue = firstMatrix.Equals(secondMatrix);
+
+            Assert.AreEqual(firstMatrix, secondMatrix);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void TestEqualsNotEqual()
+        {
+            Matrix firstMatrix = new(new int[,] { { 2, 2 }, { 2, 2 } }, 2, 2);
+            Matrix secondMatrix = new(new int[,] { { 54345, 2 }, { 2, 2 } }, 2, 2);
+
+            bool expectedValue = false;
+            bool actualValue = firstMatrix.Equals(secondMatrix);
+
+            Assert.AreNotEqual(firstMatrix, secondMatrix);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
     }
 }
