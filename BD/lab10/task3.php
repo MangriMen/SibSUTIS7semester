d9a56763dcbea4f2e78c1f16c4a585cbdd8fe649
MangriMen <49300253+MangriMen@users.noreply.github.com>
2022-09-21 00:09:24 +0700

    BD lab10 WIP

---
+++ BD/lab10/task3.php
@@ -0,0 +1,20 @@
+<style>
+    th {
+        font-family: "Arial";
+        font-size: 10pt;
+    }
+
+    td {
+        font-family: "Courier New";
+        font-size: 12pt;
+    }
+
+    h4 {
+        color: #1E88E5;
+        font-size: 14pt;
+    }
+</style>
+
+</head>
+
+<body>
