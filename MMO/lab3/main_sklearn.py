95bcb8ed8ba55e9a477df7597d6f36038fd4c858
MangriMen <49300253+MangriMen@users.noreply.github.com>
2022-09-26 02:44:00 +0700

    MMO lab3 WIP

--- MMO/lab3/main_sklearn.py
+++ MMO/lab3/main_sklearn.py
@@ -6,43 +6,58 @@ from sklearn.impute import SimpleImputer
 from sklearn.tree import DecisionTreeClassifier
 from itertools import product
 from sklearn.linear_model import LinearRegression
+from sklearn import model_selection
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+
+
+def testData(data):
+    score = []
+    for _ in range(10):
+        x = data[:, :-1]
+        y = data[:, -1]
+
+        x_train, x_test, y_train, y_test = model_selection.train_test_split(
+            x, y, test_size=0.3)
+
+        imputer = SimpleImputer()
+
+        imputer.fit(x_train)
+
+        x_train = imputer.transform(x_train)
+        x_train = preprocessing.normalize(x_train)
+
+        x_test = imputer.transform(x_test)
+        x_test = preprocessing.normalize(x_test)
+
+        regressor = LinearRegression()
+        regressor.fit(x_train, y_train)
+
+        y_predicted = regressor.predict(x_test)
+
+        score.append(sk.metrics.mean_squared_error(y_test, y_predicted))
+        # print(sk.metrics.mean_squared_error(y_test, y_predicted))
+        # print(sk.metrics.r2_score(y_test, y_predicted))
+    avg_score = 0
+    for s in score:
+        avg_score += s
+    avg_score /= 10
+
+    return avg_score
+
 
 def main():
-    data = np.genfromtxt('winequalityN.csv', delimiter=',',
-                         skip_header=True)
+    general_data = np.genfromtxt(
+        'winequalityN.csv', delimiter=',',
+        dtype=[str, float, float, float, float, float, float, float, float, float, float, float, float], skip_header=True)
 
-    X = data[:, :-1]
-    Y = data[:, -1]
+    print(general_data[:1])
+    red_wine_data = [x for x in general_data if x[0] == "red"]
+    white_wine_data = [x for x in general_data if x[0] == "white"]
 
-    X_train, X_test, Y_train, Y_test = sk.model_selection.train_test_split(X, Y, test_size=0.3)
-
-    imp = SimpleImputer()
-    imp.fit(X_train)
-    X_train = imp.transform(X_train)
-
-    regressor = LinearRegression()
-    regressor.fit(X_train, Y_train)
-
-    print(regressor.intercept_)
-    print(regressor.coef_)
-
-    with open('result.csv', 'w', newline='', encoding='utf-8') as fileOut:
-        writer = csv.writer(fileOut)
-        header = ["accuracy", "max depth", "max leaf nodes"]
-        writer.writerow(header)
-        for max_depth, max_leaf_nodes in product(range(10, 100, 10), range(10, 100, 10)):
-    #         clf = DecisionTreeClassifier(
-    #             max_depth=max_depth, max_leaf_nodes=max_leaf_nodes)
-    #         clf.fit(X_train, Y_train)
-
-            X_test = imp.transform(X_test)
-            predicted = regressor.predict(X_test)
-
-            result_row = [sk.metrics.accuracy_score(
-                Y_test, predicted), max_depth, max_leaf_nodes]
-
-            writer.writerow(result_row)
-            print(result_row)
+    print(f'General: {testData(general_data)}')
+    print(f'Red: {testData(red_wine_data)}')
+    print(f'White: {testData(white_wine_data)}')
 
 
 if __name__ == "__main__":
