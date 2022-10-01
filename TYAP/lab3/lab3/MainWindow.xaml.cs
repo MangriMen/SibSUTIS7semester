a589486815220c46e19aaaa549787217ae462ca0
MangriMen <49300253+MangriMen@users.noreply.github.com>
2022-10-01 15:49:01 +0700

    TYAP lab3 started, WIP

---
+++ TYAP/lab3/lab3/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using lab3.Helpers;
+
+namespace lab3;
+
+public sealed partial class MainWindow : WindowEx
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/WindowIcon.ico"));
+        Content = null;
+        Title = "AppDisplayName".GetLocalized();
+    }
+}
---
+++ TYAP/lab3/lab3/Views/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.ComponentModel;
+using lab3.ViewModels;
+
+using Microsoft.UI.Xaml.Controls;
+
+namespace lab3.Views;
+
+public sealed partial class MainPage : Page, INotifyPropertyChanged
+{
+    class Edge
+    {
+        public string Rule { get; set; }
+        public string State { get; set; }
+
+        public Edge(string rule="", string state="")
+        {
+            Rule = rule;
+            State = state;
+        }
+    }
+
+    List<string> _rules;
+    List<string> _alphabet;
+    string _start_rule;
+    List<string> _end_rules;
+    string _grammar;
+
+    public string Grammar
+    {
+        get => _grammar;
+        set
+        {
+            try
+            {
+                var grm = _grammar.Replace(" ", "").Split(",");
+                _rules = parseMultipleField(grm[0]);
+                _alphabet = parseMultipleField(grm[1]);
+                _start_rule = grm[2];
+                _end_rules = parseMultipleField(grm[3]);
+
+                Edges.Clear();
+                foreach (var rule in _rules) {
+                    Edges.Add(new Edge(rule, ""));
+                }
+
+                _grammar = value;
+            }
+            catch
+            {
+
+            }
+        }
+    }
+
+    List<string> parseMultipleField(string field)
+    {
+        return field.Replace(" ", "").Replace("{", "").Replace("}", "").Split(",").ToList();
+    }
+
+    List<Edge> Edges = new() { new Edge("q1", "q2"), new Edge("q2", "q3")};
+
+    Dictionary<string, List<string>> graph = new();
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public MainViewModel ViewModel
+    {
+        get;
+    }
+
+    public MainPage()
+    {
+        ViewModel = App.GetService<MainViewModel>();
+        InitializeComponent();
+    }
+}
