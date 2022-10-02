b32a6f76976ac375de9bc4a7ddabab5f02844e7d
MangriMen <49300253+MangriMen@users.noreply.github.com>
2022-10-03 03:50:05 +0700

    STP1 lab6
    * STP1 lab4 implemented matrix
    
    * STP1 lab4 Fixed Matrix Equal; Added tests
    
    * STP1 lab5 Fraction class done, left tests
    
    * PMU1 lab6 class done, left tests
    
    * PMU1 lab6 Tests started
    
    * STP1 lab6 Added tests
    
    * STP1 lab6 fixed
    
    Co-authored-by: IHiFiveI <57842399+IHiFiveI@users.noreply.github.com>

--- STP1/lab6/lab6/lab6.cpp
+++ STP1/lab6/lab6/lab6.cpp
@@ -6,4 +6,9 @@ int main()
 	auto cplx = -Complex(1, 2);
 
 	std::cout << cplx - Complex(10, 20);
+
+	std::stringstream ss;
+	ss << cplx;
+	auto actualValue = ss.str();
+	std::cout << actualValue;
 }
