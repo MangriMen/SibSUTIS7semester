d1949cd3594565693b0107c86b38471dccbfc10e
MangriMen <49300253+MangriMen@users.noreply.github.com>
2022-10-11 19:43:33 +0700

    OSRV lab3 WIP

--- OSRV/lab3/main.cpp
+++ OSRV/lab3/main.cpp
@@ -82,7 +82,9 @@ void* process_storage(Storage& storage)
 
     array<int, Storage::blocks> blocks;
     for (size_t i = 0; i < storage.blocks; i++) {
-        blocks[i] = Rect(storage.x, storage.y + (block_height + block_spacing) * i, block_width, block_height, 6, Color::Green);
+        blocks[i]
+            = Rect(storage.x, storage.y + (block_height + block_spacing) * i,
+                block_width, block_height, 6, Color::Green);
     }
 
     while (true) {
@@ -104,14 +106,17 @@ void* process_boiler(Boiler& boiler)
 
     array<int, Boiler::blocks> blocks;
     for (size_t i = 0; i < boiler.blocks; i++) {
-        blocks[i] = Rect(boiler.x, boiler.y + (block_height + block_spacing) * i, block_width, block_height, 6, Color::Yellow);
+        blocks[i]
+            = Rect(boiler.x, boiler.y + (block_height + block_spacing) * i,
+                block_width, block_height, 6, Color::Yellow);
     }
 
     int fill_counter = boiler.x > 350 ? 0 : 1;
 
     while (true) {
         if (fill_counter == 0) {
-            MsgSend(Connection::boiler_connection, &boiler, sizeof(boiler), &fill_counter, sizeof(fill_counter));
+            MsgSend(Connection::boiler_connection, &boiler, sizeof(boiler),
+                &fill_counter, sizeof(fill_counter));
         }
         // for (size_t i = 0; i < blocks.size(); i++) {
         //     Show(blocks[i]);
@@ -125,13 +130,38 @@ void* process_boiler(Boiler& boiler)
 
 void* process_car(Car& car)
 {
+    Boiler current_storage;
     Boiler current_boiler;
+    int fill_counter = 0;
+
+    tPoint carPos = GetPos(car.shape);
+    tPoint carSize = GetDim(car.shape);
+
     while (true) {
-        int recieve_msg_id = MsgReceive(Channel::boiler_channel, &current_boiler, sizeof(current_boiler), 0);
-        for (size_t i = car.x; i < current_boiler.x + 30 - 16; i += 2) {
-            MoveTo(i, car.y, car.shape);
+
+        if (fill_counter == 0) {
+            int storage_msg = MsgReceive(Channel::storage_channel,
+                &current_storage, sizeof(current_storage), 0);
+
+            carPos = GetPos(car.shape);
+            for (size_t i = carPos.x; i - current_storage.x + carSize.x / 2 > 1;
+                 i += 2 * ((i - current_storage.x) > 0 ? 1 : -1)) {
+                MoveTo(i, carPos.y, car.shape);
+                usleep(16000);
+            }
+        }
+
+        int boiler_msg = MsgReceive(Channel::boiler_channel, &current_boiler,
+            sizeof(current_boiler), 0);
+
+        carPos = GetPos(car.shape);
+        for (size_t i = carPos.x; i < current_boiler.x + carSize.x / 2;
+             i += 2) {
+            MoveTo(i, carPos.y, car.shape);
             usleep(16000);
         }
+
+        // MsgSend();
 
         usleep(16000);
     }
@@ -144,14 +174,14 @@ int main()
     Channel::boiler_channel = ChannelCreate(0);
     Channel::storage_channel = ChannelCreate(0);
 
-    Connection::boiler_connection = ConnectAttach(0, 0, Channel::boiler_channel, 0, 0);
-    Connection::storage_connection = ConnectAttach(0, 0, Channel::storage_channel, 0, 0);
+    Connection::boiler_connection
+        = ConnectAttach(0, 0, Channel::boiler_channel, 0, 0);
+    Connection::storage_connection
+        = ConnectAttach(0, 0, Channel::storage_channel, 0, 0);
 
     Line(20, 90, 80 * 6 - 20, 90, Color::White);
 
-    array<Car, 1> cars {
-        Car(0, 0, -1, Color::White)
-    };
+    array<Car, 1> cars { Car(0, 0, -1, Color::White) };
 
     array<Boiler, 4> boilers;
     for (size_t i = 0; i < boilers.size(); i++) {
@@ -159,9 +189,7 @@ int main()
         boilers[i].y = 100;
     }
 
-    array<Storage, 1> storages {
-        Storage(20, 100, 30, Color::Blue)
-    };
+    array<Storage, 1> storages { Storage(20, 100, 30, Color::Blue) };
 
     vector<pthread_t> threads(cars.size() + boilers.size() + storages.size());
 
@@ -169,15 +197,18 @@ int main()
         cars[i].x = 33;
         cars[i].y = 50;
         cars[i].shape = Rect(cars[i].x, cars[i].y, 32, 32, 6, cars[i].color);
-        pthread_create(&threads[i], NULL, (void* (*)(void*))process_car, &cars[i]);
+        pthread_create(
+            &threads[i], NULL, (void* (*)(void*))process_car, &cars[i]);
     }
 
     for (size_t i = 0; i < boilers.size(); i++) {
-        pthread_create(&threads[i + cars.size()], NULL, (void* (*)(void*))process_boiler, &boilers[i]);
+        pthread_create(&threads[i + cars.size()], NULL,
+            (void* (*)(void*))process_boiler, &boilers[i]);
     }
 
     for (size_t i = 0; i < storages.size(); i++) {
-        pthread_create(&threads[i + cars.size() + boilers.size()], NULL, (void* (*)(void*))process_storage, &storages[i]);
+        pthread_create(&threads[i + cars.size() + boilers.size()], NULL,
+            (void* (*)(void*))process_storage, &storages[i]);
     }
 
     SetColor(Color::White);
