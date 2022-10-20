2d0810eb764515b402ba2ab496cb13ac35190fd0
MangriMen <49300253+MangriMen@users.noreply.github.com>
2022-10-20 21:42:21 +0700

    TYAP lab4
    * TYAP lab4 done
    
    * TYAP lab4 fixed

---
+++ TYAP/lab4/lab4/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using lab4.Helpers;
+using Windows.Storage.Pickers;
+
+namespace lab4;
+
+public sealed partial class MainWindow : WindowEx
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/WindowIcon.ico"));
+        Content = null;
+        Title = "AppDisplayName".GetLocalized();
+    }
+}
