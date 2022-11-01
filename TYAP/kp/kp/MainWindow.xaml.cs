ec0118da39dc20171ca47ddfd7633804721c6a5c
MangriMen <49300253+MangriMen@users.noreply.github.com>
2022-11-02 03:25:55 +0700

    TYAP rgr
    * TYAP rgr started, WIP
    
    * TYAP rgr done
    
    * TYAP rgr imrpoved and fixed
    
    * TYAP rgr fixed inputs
    
    * TYAP rgr fixed
    
    * TYAP rgr added kp project
    
    * TYAP rgr kp done

---
+++ TYAP/kp/kp/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using kp.Helpers;
+
+namespace kp;
+
+public sealed partial class MainWindow : WindowEx
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/WindowIcon.ico"));
+        Content = null;
+        Title = "AppDisplayName".GetLocalized();
+    }
+}
---
+++ TYAP/kp/kp/ViewModels/ShellViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows.Input;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using kp.Contracts.Services;
+using kp.Views;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Navigation;
+
+namespace kp.ViewModels;
+
+public class ShellViewModel : ObservableRecipient
+{
+    private bool _isBackEnabled;
+
+    public ICommand MenuFileSaveCommand
+    {
+        get;
+    }
+
+    public ICommand MenuFileExitCommand
+    {
+        get;
+    }
+
+    public ICommand MenuSettingsCommand
+    {
+        get;
+    }
+
+    public ICommand MenuViewsMainCommand
+    {
+        get;
+    }
+
+    public INavigationService NavigationService
+    {
+        get;
+    }
+
+    public bool IsBackEnabled
+    {
+        get => _isBackEnabled;
+        set => SetProperty(ref _isBackEnabled, value);
+    }
+
+    public ShellViewModel(INavigationService navigationService)
+    {
+        NavigationService = navigationService;
+        NavigationService.Navigated += OnNavigated;
+
+        MenuFileSaveCommand = new RelayCommand(OnMenuFileSave);
+        MenuFileExitCommand = new RelayCommand(OnMenuFileExit);
+        MenuSettingsCommand = new RelayCommand(OnMenuSettings);
+        MenuViewsMainCommand = new RelayCommand(OnMenuViewsMain);
+    }
+
+    private void OnNavigated(object sender, NavigationEventArgs e) => IsBackEnabled = NavigationService.CanGoBack;
+
+    private void OnMenuFileSave() => MainPage.SaveGrammarToFile();
+
+    private void OnMenuFileExit() => Application.Current.Exit();
+
+    private void OnMenuSettings() => NavigationService.NavigateTo(typeof(SettingsViewModel).FullName!);
+
+    private void OnMenuViewsMain() => NavigationService.NavigateTo(typeof(MainViewModel).FullName!);
+}
