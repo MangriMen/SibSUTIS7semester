357388d3a7cb534da4176dd35d6fe4edfb4bd593
IHiFiveI <57842399+IHiFiveI@users.noreply.github.com>
2022-12-21 22:00:47 +0700

    OSRV rgr Post-merge fix

--- OSRV/rgr_hifive/task1_sp.cpp
+++ OSRV/rgr_hifive/task1_sp.cpp
@@ -42,28 +42,36 @@ void *receive_msg(void *arg)
         int msg_id = MsgReceive(Channel::benchmark, NULL, NULL, NULL);
 
         begin_times[i] = ClockCycles();
-        const int64_t CPU_FREQ = 4170000000ul;
-
-        Channel::benchmark = ChannelCreate(0);
-        Connection::benchmark = ConnectAttach(0, 0, Channel::benchmark, 0, 0);
-        pthread_t thread_1;
-        pthread_t thread_2;
-
-        pthread_create(&thread_1, NULL, send_msg, NULL);
-        pthread_create(&thread_2, NULL, receive_msg, NULL);
-
-        pthread_join(thread_1, NULL);
-        pthread_join(thread_2, NULL);
-
-        pthread_cancel(thread_1);
-        pthread_cancel(thread_2);
-
-        for (int64_t i = 0; i < TRIES; ++i)
-        {
-            Timer::result += end_times[i] - begin_times[i];
-        }
-
-        std::cout << (Timer::result / (long double)TRIES) / (long double)CPU_FREQ << " seconds" << std::endl;
-
-        return 0;
+        MsgReply(msg_id, NULL, NULL, NULL);
     }
+
+    return (void *)0;
+}
+
+int main()
+{
+    const int64_t CPU_FREQ = 4170000000ul;
+
+    Channel::benchmark = ChannelCreate(0);
+    Connection::benchmark = ConnectAttach(0, 0, Channel::benchmark, 0, 0);
+    pthread_t thread_1;
+    pthread_t thread_2;
+
+    pthread_create(&thread_1, NULL, send_msg, NULL);
+    pthread_create(&thread_2, NULL, receive_msg, NULL);
+
+    pthread_join(thread_1, NULL);
+    pthread_join(thread_2, NULL);
+
+    pthread_cancel(thread_1);
+    pthread_cancel(thread_2);
+
+    for (int64_t i = 0; i < TRIES; ++i)
+    {
+        Timer::result += end_times[i] - begin_times[i];
+    }
+
+    std::cout << (Timer::result / (long double)TRIES) / (long double)CPU_FREQ << " seconds" << std::endl;
+
+    return 0;
+}
